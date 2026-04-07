4e311faebf9a62b02765939e741bf5c6af91fd36
Nicole Ellis <nell0012@student.monash.edu>
2026-04-08 04:36:02 +1000

    Manually added scrim results

---
+++ main.py
@@ -0,0 +1,64 @@
+matches = [ 
+    { 
+        "date": "2026-02-06", 
+        "opponent": "swaglord9000", 
+        "match_type": "scrim", 
+        "map": "Bind", 
+        "result": "Victory", 
+        "team_score": 16, 
+        "enemy_score": 8, 
+        "players": [ 
+            
+            { 
+            "name": "Gek", 
+            "agent": "Raze", 
+            "kills": 27, 
+            "deaths": 19, "assists": 7, 
+            "ACS": 335, 
+            "first_bloods": 6 
+            }, 
+            
+            { 
+            "name": "Josh", 
+            "agent": "Brimstone", 
+            "kills": 26, 
+            "deaths": 14, 
+            "assists": 5, 
+            "ACS": 261, 
+            "first_bloods": 1 
+            }, 
+            
+            { 
+            "name": "Jono", 
+            "agent": "Viper", 
+            "kills": 16, 
+            "deaths": 19, 
+            "assists": 6, 
+            "ACS": 221, 
+            "first_bloods": 5 
+            }, 
+            
+            { 
+            "name": "Jamie", 
+            "agent": "Yoru", 
+            "kills": 15, 
+            "deaths": 12, 
+            "assists": 3, 
+            "ACS": 178, 
+            "first_bloods": 2 
+            }, 
+            
+            { 
+            "name": "Walli", 
+            "agent": "Fade", 
+            "kills": 13, 
+            "deaths": 13, 
+            "assists": 6, 
+            "ACS": 132, 
+            "first_bloods": 1 
+            }, 
+        ] 
+    } 
+] 
+
+print(matches)
